423f1f0001fecfa3b97b5aac235be09693849429
Alejandro Varona <avarona10@gmail.com>
2020-02-11 11:01:44 -0500

    remove yarn & add android

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,15 +7,13 @@ export PATH=/usr/local/share/npm/bin:$PATH
 # Homebrew installed
 export PATH="/usr/local/sbin:$PATH"
 
-# Yarn installed through homebrew
-export PATH="$PATH:`yarn global bin`"
-
 # ASDF installed through repo
 source $HOME/.asdf/asdf.sh
 source $HOME/.asdf/completions/asdf.bash
 
 # Android development
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK=$ANDROID_SDK/lib
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
